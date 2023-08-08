4706c6d16c6aa88f130b42d431339d1c34342c17
hye914 <kimhyesu0914@gmail.com>
2023-08-08 13:05:52 +0900

    feat: <>움직임

--- src/components/MainPage/Banner.jsx
+++ src/components/MainPage/Banner.jsx
@@ -1,45 +1,83 @@
-import { useEffect, useState } from "react"
-import axios from "../../api/axios"
-import requests from "../../api/requests"
+import { useEffect, useState } from "react";
+import axios from "../../api/axios";
+import requests from "../../api/requests";
 import {
-    BannerContainer, BannerContents, BannerTitle, BannerButton, BannerPlay, BannerInfo, BannerDes, BannerFadeBottom, BannerPlayContainer, BannerIframe
-} from './Styled'
+  BannerContainer,
+  BannerContents,
+  BannerTitle,
+  BannerButton,
+  BannerPlay,
+  BannerInfo,
+  BannerDes,
+  BannerFadeBottom,
+  BannerPlayContainer,
+  BannerIframe,
+} from "./Styled";
 
 export default function Banner() {
-    
-    const [movie, setMovie] = useState([])
+  const [movie, setMovie] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
 
-    // if(!isClicked) {
-        return(
-            <BannerContainer
-                movie={movie.backdrop_path}
-                style={{backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-            }}
-            >
-                <BannerContents>
-                    <BannerTitle>
-                    {movie.title || movie.name || movie.original_name}
-                    </BannerTitle>
-                    <BannerButton>
-                        <BannerPlay>Play</BannerPlay>
-                        <BannerInfo>More Information</BannerInfo>
-                    </BannerButton>
-                    <BannerDes>{movie.overview}</BannerDes>
-                </BannerContents>
-                <BannerFadeBottom />
-            </BannerContainer>
-        )
-    // } else {
-    //     return(
-    //         <BannerPlayContainer>
-    //             <BannerIframe
-    //                 width="640"
-    //                 height="360"
-    //                 src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=&autoplay=1&mute=1&playlist=${movie.videos.results[0].key}`}
-    //                 title="YouTube video player" 
-    //                 frameborder="0" 
-    //                 allow=" autoplay; fullscreen" allowFullScreen
-    //             />
-    //         </BannerPlayContainer>
-    // )}
+  useEffect(() => {
+    fetchMovie();
+  }, []);
+
+  //비동기 함수
+  const fetchMovie = async () => {
+    const request = await axios.get(requests.fetchNowPlaying);
+
+    const movieId =
+      request.data.results[
+        Math.floor(Math.random() * request.data.results.length)
+      ].id;
+
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response: "videos" },
+    });
+
+    setMovie(movieDetail);
+  };
+
+  console.log(movie);
+  // 문자열을 자를 함수
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }; //optional changing?
+
+  if (!isClicked) {
+    return (
+      <BannerContainer
+        movie={movie.backdrop_path}
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        }}
+      >
+        <BannerContents>
+          <BannerTitle>
+            {movie.title || movie.name || movie.original_name}
+          </BannerTitle>
+          <BannerButton>
+            <BannerPlay onClick={() => setIsClicked(true)}>Play</BannerPlay>
+            <BannerInfo>More Information</BannerInfo>
+          </BannerButton>
+          <BannerDes>{truncate(movie?.overview, 100)}</BannerDes>
+        </BannerContents>
+        <BannerFadeBottom />
+      </BannerContainer>
+    );
+  } else {
+    return (
+      <BannerPlayContainer>
+        <BannerIframe
+          width="640"
+          height="360"
+          src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=&autoplay=1&mute=1&playlist=${movie.videos.results[0].key}`}
+          title="YouTube video player"
+          frameborder="0"
+          allow=" autoplay; fullscreen"
+          allowFullScreen
+        />
+      </BannerPlayContainer>
+    );
+  }
 }
--- src/components/MainPage/Row.jsx
+++ src/components/MainPage/Row.jsx
@@ -1,32 +1,70 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "../../api/axios";
 import MovieModal from "../MovieModal/MovieModal";
-import {
-    RowContainer, RowPosters, RowPoster, RowTitle
-} from './Styled'
+import { RowContainer, RowPosters, RowPoster, RowTitle } from "./Styled";
+
+// Swiper
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Navigation } from "swiper/modules";
 
 export default function Row({ isLarge, title, id, fetchUrl }) {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
+
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchUrl]);
+
+  const fetchMovieData = async () => {
+    try {
+      const response = await axios.get(fetchUrl);
+      setMovies(response.data.results);
+    } catch (error) {
+      console.error("Error fetching movie data:", error);
+    }
+  };
 
   return (
     <>
-    <RowContainer>
-      <RowTitle>{title}</RowTitle>
-      <RowPosters>
-        {movies.map((movie)=>(
-          <RowPoster
-            key={movie.id}
-            isLarge={isLarge ? 'true' : 'false'}
-            src={`https://image.tmdb.org/t/p/original/${
-                isLarge
-                    ? movie.poster_path
-                    : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        ))}
-      </RowPosters>
-    </RowContainer>
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
+      <RowContainer>
+        <RowTitle>{title}</RowTitle>
+        <Swiper
+          slidesPerView={title === "NETFLIX ORIGINALS" ? 13 : 6}
+          spaceBetween={20}
+          navigation={true}
+          modules={[Navigation]}
+          className="Swiper"
+        >
+          {movies.map((movie) => (
+            <SwiperSlide key={movie.id}>
+              <RowPoster
+                isLarge={isLarge ? "true" : "false"}
+                src={`https://image.tmdb.org/t/p/original/${
+                  isLarge ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+                onClick={() => handleClick(movie)}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </RowContainer>
     </>
-  )
+  );
 }
